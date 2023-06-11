4d623970792ca2f754a6052c15c0ed1079fd023b
Lewis <lewisdennisevans@hotmail.com>
2023-06-11 19:24:20 +0100

    CrazyDisco-devBuild #0.00b

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import wavelink
 logger = settings.logging.getLogger(__name__)
 
 class MusicBot(commands.Cog):
+    vc : wavelink.Player = None
     def __init__(self, bot):
         self.bot = bot
     async def setup(self):
@@ -21,8 +22,16 @@ class MusicBot(commands.Cog):
 
     @commands.command()
     async def join(self, ctx):
+        channel =ctx.message.author.voice.channel
+        if channel:
+            self.vc = channel.connect(cls=wavelink.Player)
+            await ctx.send(f"Connected to {channel.name}")
     @commands.command()
-    async def add(self, ctx):
+    async def add(self, ctx, *title str):
+
+    @commands.command()
+    async def play(self, ctx):
+
 
 
 
